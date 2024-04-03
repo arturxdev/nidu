58e75a5996cd66a0d389811900465df84b90df09
Arturo GUerrero <arturo.guerrero@koibanx.com>
2024-04-03 13:26:27 -0600

    feat: add openapi

---
+++ src/app/docs/page.tsx
@@ -0,0 +1,42 @@
+'use client';
+import SwaggerUI from "swagger-ui-react"
+import "swagger-ui-react/swagger-ui.css"
+
+import { z } from 'zod';
+import { ZodOpenApiOperationObject, createDocument, extendZodWithOpenApi } from 'zod-openapi';
+
+extendZodWithOpenApi(z);
+
+const file = z.string().openapi({
+  description: 'Job title',
+  example: 'My job',
+});
+const proccessFile: ZodOpenApiOperationObject = {
+  operationId: 'proccessBbvaCredit',
+  summary: 'Get Job',
+  responses: {
+    '200': {
+      description: 'Successful operation',
+      content: {
+        'multipart/form-data': { schema: z.object({ file }) },
+      },
+    },
+  },
+};
+
+const document = createDocument({
+  openapi: '3.1.0',
+  info: {
+    title: 'Nidu API',
+    version: '1.0.0',
+  },
+  paths: {
+    '/process/bbva/credit': {
+      post: proccessFile
+    },
+    '/process/bbva/debit': {
+      post: proccessFile
+    },
+  },
+});
+export default function App() { return <SwaggerUI spec={document} /> }
