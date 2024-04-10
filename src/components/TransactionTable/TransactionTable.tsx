0346ac55ad701d01e9f865c54c97206bcaf881c5
Arturo GUerrero <arturo.guerrero@koibanx.com>
2024-04-10 10:08:34 -0600

    fix: error transactions

--- src/components/TransactionTable/TransactionTable.tsx
+++ src/components/TransactionTable/TransactionTable.tsx
@@ -1,10 +1,12 @@
 import { transactionService } from "@/services/transaction";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
+import { validateRequest } from "@/lib/auth";
 
 export default async function TransactionTable() {
-  const transactions = await transactionService.get();
-
+  const { user, session } = await validateRequest();
+  if (!user) return null
+  const transactions = await transactionService.get(10, 0, 'desc', user.id)
   return (
     <div className="">
       <DataTable columns={columns} data={transactions.results} />
