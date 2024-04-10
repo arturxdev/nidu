f5dac1a646ef0cd95e489fabdabbfb545bafaead
Brandon Reyes <brandonreyes@MacBook-Air-de-Brandon.local>
2024-04-09 23:08:51 -0600

    merge fixed

--- src/services/transaction.ts
+++ src/services/transaction.ts
@@ -4,29 +4,43 @@ import connectMongo from "@/lib/mongoose";
 import { TransactionModel } from "@/models/transaccion";
 import { SortOrder } from "mongoose";
 
-
 export const transactionService = {
   create: async (data: Transaction) => {
-    await connectMongo()
-    return TransactionModel.create(data)
+    await connectMongo();
+    return TransactionModel.create(data);
   },
   update: async (transactionId: string, data: Partial<Transaction>) => {
-    await connectMongo()
-    return TransactionModel.updateOne({ _id: transactionId }, { descriptionUser: data.descriptionUser, category: data.category })
+    await connectMongo();
+    return TransactionModel.updateOne(
+      { _id: transactionId },
+      { descriptionUser: data.descriptionUser, category: data.category }
+    );
   },
-  get: async ({ limit, order, page }: Filter, userId: string) => {
-    await connectMongo()
-    const transactionsTotal = await TransactionModel.find({ where: { userId } })
-    const transactions = await TransactionModel.find({ where: { userId } }).skip(page).limit(limit).sort({ date: order as SortOrder })
+  get: async (
+    limit: number = 10,
+    order: string = "desc",
+    page: number = 0,
+    userId: string
+  ) => {
+    await connectMongo();
+    const transactionsTotal = await TransactionModel.find({
+      where: { userId },
+    });
+    const transactions = await TransactionModel.find({ where: { userId } })
+      .skip(page)
+      .limit(limit)
+      .sort({ date: order as SortOrder });
     const transformedResults = transactions.map((result: any) => {
-      const { _doc: { _id, __v, ...restDataUser } } = result;
+      const {
+        _doc: { _id, __v, ...restDataUser },
+      } = result;
       return { id: _id.toString(), ...restDataUser };
     });
     return {
       total: transactionsTotal.length,
       limit,
       page,
-      results: transformedResults
-    }
-  }
-}
+      results: transformedResults,
+    };
+  },
+};
